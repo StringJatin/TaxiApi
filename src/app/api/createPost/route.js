fce11cf839c6c7b8ce261e9feb5034e039720538
Jatin chouhan <jatinchouhan1515@gmail.com>
2023-09-12 22:21:39 +0530

    fixed post

--- src/app/api/createPost/route.js
+++ src/app/api/createPost/route.js
@@ -1,32 +1,70 @@
+// import dbConn from "@/utils/dbConn";
+// import PostPage from "@/models/post"
+// import {NextResponse} from "next/server";
+// import cors from "cors";
+// export const revalidate = 1;
+
+// export async function POST(req, res) {
+//     try {
+
+//         const body = await req.json();
+//         await dbConn();
+
+//        const res = await PostPage.create(body);
+//         return NextResponse.json({
+//             message:"blog sent successfully!"
+//         }, {
+//             status: 200,
+//             headers: {
+//                 'Access-Control-Allow-Origin': '*',
+//                 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+//                 'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+//               },
+//         })
+
+//     }catch (e) {
+//         return NextResponse.json(
+//             { message: "Server error, please try again!" },
+//             { status: 500 }
+//         )
+//     }
+// }
+
 import dbConn from "@/utils/dbConn";
-import PostPage from "@/models/post"
-import {NextResponse} from "next/server";
-import cors from "cors";
+import PostPage from "@/models/post";
+import { NextResponse } from "next/server";
+import Cors from "cors";
+import initMiddleware from "@/utils/init-middleware";
+
 export const revalidate = 1;
 
-export async function POST(req, res) {
-    try {
+// Initialize the cors middleware
+const corsMiddleware = initMiddleware(
+  Cors({
+    // Specify allowed origins or use '*' for any origin
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-        const body = await req.json();
-        await dbConn();
+export default async function handler(req, res) {
+  // Apply the cors middleware to your route
+  await corsMiddleware(req, res);
 
-       const res = await PostPage.create(body);
-        return NextResponse.json({
-            message:"blog sent successfully!"
-        }, {
-            status: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-              },
-        })
+  try {
+    const body = await req.json();
+    await dbConn();
 
-    }catch (e) {
-        return NextResponse.json(
-            { message: "Server error, please try again!" },
-            { status: 500 }
-        )
-    }
+    // Use a different variable name for the result
+    const result = await PostPage.create(body);
+
+    return res.status(200).json({
+      message: "Blog sent successfully!",
+    });
+  } catch (e) {
+    return res.status(500).json({
+      message: "Server error, please try again!",
+    });
+  }
 }
-
