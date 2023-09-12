1c2136553f62fce9b23831f31f14ae77f8b6867f
Jatin chouhan <jatinchouhan1515@gmail.com>
2023-09-12 22:13:39 +0530

    modified:   src/app/api/createPost/route.js

--- src/app/api/createPost/route.js
+++ src/app/api/createPost/route.js
@@ -1,67 +1,32 @@
-// import dbConn from "@/utils/dbConn";
-// import PostPage from "@/models/post"
-// import {NextResponse} from "next/server";
-// import cors from "cors";
-// export const revalidate = 1;
-
-// export async function POST(req, res) {
-//     try {
-
-//         const body = await req.json();
-//         await dbConn();
-
-//        const res = await PostPage.create(body);
-//         return NextResponse.json({
-//             message:"blog sent successfully!"
-//         }, {
-//             status: 200,
-//             headers: {
-//                 'Access-Control-Allow-Origin': '*',
-//                 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-//                 'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-//               },
-//         })
-
-//     }catch (e) {
-//         return NextResponse.json(
-//             { message: "Server error, please try again!" },
-//             { status: 500 }
-//         )
-//     }
-// }
-
 import dbConn from "@/utils/dbConn";
-import PostPage from "@/models/post";
-import { NextResponse } from "next/server";
-import cors from "cors"; // Import the cors middleware
-
+import PostPage from "@/models/post"
+import {NextResponse} from "next/server";
+import cors from "cors";
 export const revalidate = 1;
 
-// Use the cors middleware to handle CORS headers
-const corsMiddleware = cors();
+export async function POST(req, res) {
+    try {
 
-export default async function handler(req, res) {
-  // Apply CORS middleware
-  corsMiddleware(req, res);
+        const body = await req.json();
+        await dbConn();
 
-  try {
-    const body = await req.json();
-    await dbConn();
+       const res = await PostPage.create(body);
+        return NextResponse.json({
+            message:"blog sent successfully!"
+        }, {
+            status: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+              },
+        })
 
-    const response = await PostPage.create(body);
-
-    return NextResponse.json(
-      {
-        message: "blog sent successfully!",
-      },
-      {
-        status: 200,
-      }
-    );
-  } catch (e) {
-    return NextResponse.json(
-      { message: "Server error, please try again!" },
-      { status: 500 }
-    );
-  }
+    }catch (e) {
+        return NextResponse.json(
+            { message: "Server error, please try again!" },
+            { status: 500 }
+        )
+    }
 }
+
