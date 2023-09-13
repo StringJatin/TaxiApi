e69e572832921975c07ee52e74ce8aa2003b6458
Jatin chouhan <jatinchouhan1515@gmail.com>
2023-09-13 14:01:11 +0530

    change in formadata

--- src/app/api/formdata/route.js
+++ src/app/api/formdata/route.js
@@ -1,37 +1,71 @@
-
+import Cors from 'cors';
+import { NextApiRequest, NextApiResponse } from 'next';
+import initMiddleware from '@/utils/init-middleware';
 import dbConn from "@/utils/dbConn";
 import FormData from "@/models/form";
 import {NextResponse} from "next/server";
 export const revalidate = 1;
-export async function POST(req, res) {
+// export async function POST(req, res) {
 
 
-    try {
+//     try {
 
-        const body = await req.json();
-        await dbConn();
+//         const body = await req.json();
+//         await dbConn();
 
-        await FormData.create(body);
+//         await FormData.create(body);
 
-        return NextResponse.json({
-            message:"Message sent successfully!"
-        }, {
-            status: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-              },
-        })
+//         return NextResponse.json({
+//             message:"Message sent successfully!"
+//         }, {
+//             status: 200,
+//             headers: {
+//                 'Access-Control-Allow-Origin': '*',
+//                 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+//                 'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+//               },
+//         })
 
-    }catch (e) {
-        return NextResponse.json(
-            { message: "Server error, please try again!" },
-            { status: 500 }
-        )
+//     }catch (e) {
+//         return NextResponse.json(
+//             { message: "Server error, please try again!" },
+//             { status: 500 }
+//         )
+//     }
+// }
+const cors = initMiddleware(
+    Cors({
+      methods: ['POST', 'OPTIONS'], // Allow POST and OPTIONS methods
+      origin: 'https://your-allowed-origin.com', // Replace with your allowed origin
+      optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+    })
+  );
+  
+  export default async function handler(req, res) {
+    // Apply the CORS middleware for both POST and OPTIONS requests
+    await cors(req, res);
+  
+    if (req.method === 'OPTIONS') {
+      // Handle preflight (OPTIONS) request for CORS
+      return res.status(204).end();
     }
-}
-
+  
+    try {
+      const body = JSON.parse(req.body); // Parse the request body if needed
+      await dbConn();
+  
+      const result = await FormData.create(body);
+  
+      return res.status(200).json({
+        message: 'Blog sent successfully!',
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        message: 'Server error, please try again!',
+      });
+    }
+  }
 export const GET = async (Request) => {
    
     try {
